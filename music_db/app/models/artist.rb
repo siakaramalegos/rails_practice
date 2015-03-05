ca30f72bfd40d1d72328ddb473f079042e546037
Sia <siakaramalegos@gmail.com>
2015-03-05 10:08:17 -0600

    Add validation to music db

--- music_db/app/models/artist.rb
+++ music_db/app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ActiveRecord::Base
   belongs_to :genre
   has_many :songs
+
+  validates :name, length: {minimum: 3}, uniqueness: true, presence: true
 end
--- music_db/app/models/genre.rb
+++ music_db/app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ActiveRecord::Base
   has_many :artists
   has_many :songs#, through :artists
+
+  validates :name, length: {minimum: 3}, uniqueness: true, presence: true
 end
