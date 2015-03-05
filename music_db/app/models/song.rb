0e25ef766fb9df67d574e8698dbf524441aee636
Sia <siakaramalegos@gmail.com>
2015-03-05 13:51:11 -0600

    Fix validation errors

--- music_db/app/models/song.rb
+++ music_db/app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  validates :genre_id, presence: true
+  validates :artist_id, presence: true
   validates :name, length: {minimum: 3}, uniqueness: true, presence: true
 end
