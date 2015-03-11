78b334d8d823d6f8a318684520a9149338bf9671
Sia <siakaramalegos@gmail.com>
2015-03-11 10:09:20 -0500

    Add basic content and bootstrap

--- travel/app/controllers/welcome_controller.rb
+++ travel/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @homeland = 'USA'
+    @countries = ['Greece','India','China','Spain','Bolivia','Peru','Costa Rica','Italy','The Philippines', 'England','Mexico']
   end
 
   def about
